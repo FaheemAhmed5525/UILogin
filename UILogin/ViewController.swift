8745af3557ab6d380c0d7fe1351fed2aab64b5b7
Faheem Ahmed <faheemahmed5525@gmail.com>
2024-07-10 15:30:19 +0500

    Initial Commit

---
+++ UILogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UILogin
+//
+//  Created by Faheeam Ahmed on 10/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
